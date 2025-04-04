03b284f1f28e85de724ac28ec3ee19a404ec1221
Will Zakroff <willzakroff@gmail.com>
2025-04-04 14:38:42 -0400

    Added nav-bar and footer functionality

--- web-dev-site/app/layout.tsx
+++ web-dev-site/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import Navbar from "@/app/components/navbar"; // Ensure this is imported to use in the layout if needed
+import Footer from "@/app/components/footer"; // Ensure this is imported to use in the layout if needed
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
